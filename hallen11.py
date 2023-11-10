ba67611e01fa92f528cd9e7b62de29c01de88432
haylee <haylee313@sbcglobal.net>
2023-11-10 03:33:47 +0000

    adding lab 11

---
+++ hallen11.py
@@ -0,0 +1,15 @@
+#Haylee Allen
+#Lab #11
+#CSC-321
+
+def numSquared (num):
+    return (num * num)
+
+sq2 = int(input("Please enter a number of your choosing: "))
+
+squaringNum = numSquared(sq2)
+
+print("The number you entered, when squared, equals: ", squaringNum, "- Batman > Superman, and that is a fact!")
+
+
+
